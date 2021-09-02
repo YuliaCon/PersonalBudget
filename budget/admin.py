794939d24710ed6e4d7404f0312d52b63c092882
Yulia <58860784+YuliaCon@users.noreply.github.com>
2021-09-02 11:07:14 -0500

    Add files via upload

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register AccountInfo model.
+from .models import AccountInfo
+admin.site.register(AccountInfo)
+
+# Register Incomes model
+from .models import Income
+admin.site.register(Income)
+
+#Register Expenses model
+
+from  .models import Expense
+admin.site.register(Expense)
